3059bceaba90feb5a05db1ea4905858361347d8f
jo33200 <jo33200@gmail.com>
2024-09-20 16:11:13 +0200

    Refactor App component to include react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,31 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/footer' 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/home/home'
+import Ad from './pages/ad/ad'
+import Quote from './pages/quote/quote'
+import Contact from './pages/contact/contact'
+import Rates from './pages/rates/rates'
 
 function App() {
   
 
   return (
-    <>
+    <Router>
       <div>
         <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ad" element={<Ad />} />
+          <Route path="/catalog" element={<Quote />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/rates" element={<Rates />} />
+        </Routes>
         <Footer />
       </div>
-    </>
+    </Router>
   )
 }
 
