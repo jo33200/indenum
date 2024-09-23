916470e071fa16eb013590e28f55f8c94dd7e225
jo33200 <jo33200@gmail.com>
2024-09-23 17:54:23 +0200

    Refactor Home component to add Carousel and update page title

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,13 @@
-const Home = () => {
-    return (
-      <div>
-        <h1>Bienvenue</h1>
-      </div>
-    );
-  };
-  
-  export default Home;
+import Carousel from '../../components/Carousel';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center my-8">Bienvenue sur notre site</h1>
+      <Carousel />
+      {/* Autres contenus de la page d'accueil */}
+    </div>
+  );
+};
+
+export default HomePage;
