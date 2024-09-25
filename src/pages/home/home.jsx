205470ecd8dc4c2eddbd5671d81a096288781605
jo33200 <jo33200@gmail.com>
2024-09-25 15:16:23 +0200

    Refactor home page to add CardHome component to the Carousel

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import Carousel from '../../components/Carousel';
+import CardHome from '../../components/CardHome';
 
 const HomePage = () => {
   return (
-    <div className='my-8'>
+    <div className=' bg-gradient-to-b from-[#E37E21] to-[#ffffff]'>
       <Carousel />
-    </div>
+      <CardHome />
+    </div> 
   );
 };
 
