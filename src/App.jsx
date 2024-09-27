418aec03431aebd6806d3f0e697bae565271d5eb
jo33200 <jo33200@gmail.com>
2024-09-27 12:31:05 +0200

    Refactor App.jsx to fix import casing and add wildcard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
 import './App.css'
 import Header from './components/Header'
-import Footer from './components/footer' 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer' 
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 import Home from './pages/home/home'
 import Ad from './pages/ad/ad'
@@ -23,6 +23,7 @@ function App() {
           <Route path="/quote" element={<Quote />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/rates" element={<Rates />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
