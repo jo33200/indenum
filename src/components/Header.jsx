02d50782efbbae5ab9b4de33d85c6c0fd7713992
jo33200 <jo33200@gmail.com>
2024-10-14 17:18:24 +0200

    Refactor Header component to improve responsiveness and add hover effects

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,29 +8,29 @@ const Header = () => {
     <div className="w-full flex flex-col justify-center items-center gap-2">
       {/* Logo */}
       <div>
-        <img src={Logo} alt="Indenum" />
+        <img src={Logo} alt="Indenum" className='w-48 h-auto md:w-60 lg:w-72 xl:w-80' />
       </div>
 
         {/* Slogan */}
-      <p className="text-white font-semibold container w-3/4 text-xs">Service de Réparation pour vos objets électroniques</p>
+      <p className="text-white sm:text-sm md:text-base font-semibold container w-3/4 text-xs">Service de Réparation pour vos objets électroniques</p>
 
       {/* Navigation */}
       <nav className="w-full mt-2">
-        <ul className="w-full h-10 flex justify-center items-center gap-3 bg-neutral-700">
-          <li>
-            <Link to="/" className="text-white font-semibold hover:text-gray-300">Accueil</Link>
+        <ul className="w-full h-10 text-sm sm:text-base md:text-lg flex justify-center items-center gap-3 sm:gap-6 lg:gap-14 bg-neutral-700">
+          <li className='hover:scale-110 transition-transform duration-200'>
+            <Link to="/" className="text-white font-semibold hover:text-gray-300 ">Accueil</Link>
           </li>
-          <li>
-            <Link to="/rates" className="text-white font-semibold hover:text-gray-300">Tarifs</Link>
+          <li className='hover:scale-110 transition-transform duration-200'>
+            <Link to="/rates" className="text-white font-semibold hover:text-gray-300 hover:scale-110 transition-transform duration-200">Tarifs</Link>
           </li>
-          <li>
-            <Link to="/ad" className="text-white font-semibold hover:text-gray-300">Annonces</Link>
+          <li className='hover:scale-110 transition-transform duration-200'>
+            <Link to="/ad" className="text-white font-semibold hover:text-gray-300 hover:scale-110 transition-transform duration-200">Annonces</Link>
           </li>
-          <li>
-            <Link to="/quote" className="text-white font-semibold hover:text-gray-300">Devis</Link>
+          <li className='hover:scale-110 transition-transform duration-200'>
+            <Link to="/quote" className="text-white font-semibold hover:text-gray-300 hover:scale-110 transition-transform duration-200">Devis</Link>
           </li>
-          <li>
-            <Link to="/contact" className="text-white font-semibold hover:text-gray-300">Contact</Link>
+          <li className='hover:scale-110 transition-transform duration-200'>
+            <Link to="/contact" className="text-white font-semibold hover:text-gray-300 hover:scale-110 transition-transform duration-200">Contact</Link>
           </li>
         </ul>
       </nav>
