2fbb47062b04b18ebba3dd94f3b98e95903f1b9e
jo33200 <jo33200@gmail.com>
2024-10-16 11:57:15 +0200

    Refactor home page component to update layout and imports

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,12 @@
-import Carousel from '../../components/Carousel';
-import CardHome from '../../components/CardHome';
+import Carousel from "../../components/Carousel";
+import CardHome from "../../components/CardHome";
 
 const HomePage = () => {
   return (
-    <div className=' bg-gradient-to-b from-[#E37E21] to-[#ffffff] h-auto'>
+    <div className="h-auto w-full bg-gradient-to-b from-[#E37E21] to-[#ffffff]">
       <Carousel />
       <CardHome />
-    </div> 
+    </div>
   );
 };
 
