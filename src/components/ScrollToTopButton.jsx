b82d9bc92d0e5fcc16332e9c047ca36ccb6f994e
jo33200 <jo33200@gmail.com>
2024-10-17 15:47:34 +0200

    Refactor ScrollToTopButton component to improve code readability and consistency

--- src/components/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ScrollToTopButton = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -12,23 +12,23 @@ const ScrollToTopButton = () => {
   };
 
   const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
     <button
       onClick={scrollToTop}
-      className={`w-64 fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-name-orange text-white p-1 rounded-full shadow-lg transition-opacity duration-300 ${
-        isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'
+      className={`fixed bottom-4 left-1/2 w-64 -translate-x-1/2 transform rounded-full bg-name-orange p-1 text-white shadow-lg transition-opacity duration-300 ${
+        isVisible ? "opacity-100" : "pointer-events-none opacity-0"
       }`}
-      style={{ display: isVisible ? 'block' : 'none' }}
+      style={{ display: isVisible ? "block" : "none" }}
     >
       ↑ vers le haut
     </button>
