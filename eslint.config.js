a392e3477161cdbf1e747681322cbd27cfca8b34
jo33200 <jo33200@gmail.com>
2024-10-17 10:43:22 +0200

    Refactor eslint.config.js to disable react/react-in-jsx-scope rule and update import statements

--- eslint.config.js
+++ eslint.config.js
@@ -5,11 +5,14 @@ import pluginReact from "eslint-plugin-react";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser },
-    rules: {
-    "react/react-in-jsx-scope": "off" 
-    },
+  {
+    languageOptions: { globals: globals.browser },
   },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  { 
+  rules: {
+    "react/react-in-jsx-scope": "off" 
+    },
+  }
 ];
