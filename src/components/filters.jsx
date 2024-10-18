754acead613679328b1bde99e043a9c9a0629528
jo33200 <jo33200@gmail.com>
2024-10-18 15:58:10 +0200

    Refactor Filters component to improve layout and add responsive behavior

--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 const Filters = ({
   filterData,
@@ -9,6 +9,7 @@ const Filters = ({
   setOpenCategory,
 }) => {
   const [openCategories, setOpenCategories] = useState({});
+  const [showAllFilters, setShowAllFilters] = useState(false);
 
   useEffect(() => {
     if (openCategory) {
@@ -37,66 +38,91 @@ const Filters = ({
   };
 
   return (
-    <div className="mb-4 flex w-full justify-around bg-gray-100 p-1 sm:flex-col sm:justify-between sm:rounded-md sm:p-4 sm:shadow-md">
-      {filterData.map((filter) => (
-        <div key={filter.category} className="mb-2">
-          <div
-            className="flex cursor-pointer items-center justify-between gap-1 rounded sm:bg-gray-200 sm:p-2"
-            onClick={() => toggleCategory(filter.category)}
-          >
-            <h3 className="text-lg font-bold">{filter.category}</h3>
-            <span className="text-xl">
-              <span className="block text-xs sm:hidden">
-                {openCategories[filter.category] ? "▲" : "▼"}
-              </span>
-              <span className="hidden sm:block">
+    <div className="flex w-full sm:justify-around sm:bg-gray-100 md:flex-col md:justify-between md:rounded-md md:p-4 md:shadow-md">
+      {/* Affichage du bouton "Filtres" pour les petits écrans */}
+      <div className="w-1/2 sm:hidden">
+        <div
+          className="flex cursor-pointer items-center justify-between bg-gray-200 rounded-t p-2"
+          onClick={() => setShowAllFilters(!showAllFilters)} // Ouvrir/fermer tous les filtres
+        >
+          <h3 className="text-lg font-bold">Filtres</h3>
+          <span className="text-2xl font-bold text-gray-700">
+            {showAllFilters ? "-" : "+"} {/* Icone pour ouvrir/fermer */}
+          </span>
+        </div>
+
+        {/* Afficher toutes les catégories lorsque "Filtres" est ouvert */}
+        {showAllFilters && (
+          <div className="">
+            {filterData.map((filter) => (
+              <div key={filter.category} className="">
+                <div
+                  className="flex cursor-pointer items-center justify-between gap-1 bg-gray-100 p-2"
+                  onClick={() => toggleCategory(filter.category)}
+                >
+                  <h3 className="text-sm font-bold">{filter.category}</h3>
+                  <span className="text-xl text-gray-500">
+                    {openCategories[filter.category] ? "-" : "+"}
+                  </span>
+                </div>
+                {openCategories[filter.category] && (
+                  <div className="text-left">
+                    {filter.subcategories.map((subcategory) => (
+                      <div key={subcategory} className="ml-4 mt-2">
+                        <label className="inline-flex items-start">
+                          <input
+                            type="checkbox"
+                            value={subcategory}
+                            checked={selectedFilters.includes(subcategory)}
+                            onChange={(e) => onFilterChange(e.target.value)}
+                            className="form-checkbox"
+                          />
+                          <span className="ml-2">{subcategory}</span>
+                        </label>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      {/* Affichage des catégories pour les écrans plus grands */}
+      <div className="hidden sm:block">
+        {filterData.map((filter) => (
+          <div key={filter.category} className="mb-2">
+            <div
+              className="flex cursor-pointer items-center justify-between gap-1 rounded bg-gray-200 p-2"
+              onClick={() => toggleCategory(filter.category)}
+            >
+              <h3 className="text-lg font-bold">{filter.category}</h3>
+              <span className="text-xl">
                 {openCategories[filter.category] ? "−" : "+"}
               </span>
-            </span>
-          </div>
-
-          {openCategories[filter.category] && (
-            <div className="text-left">
-              {filter.category === "Console"
-                ? filter.subcategories.map((subcat) => (
-                    <div key={subcat.subcategory} className="ml-4 bg-gray-100">
-                      <h4 className="text-md font-semibold">
-                        {subcat.subcategory}
-                      </h4>
-                      {subcat.subSubcategories.map((subSubcat) => (
-                        <div key={subSubcat} className="ml-6 mt-2">
-                          <label className="inline-flex items-start">
-                            <input
-                              type="checkbox"
-                              value={subSubcat}
-                              checked={selectedFilters.includes(subSubcat)}
-                              onChange={(e) => onFilterChange(e.target.value)}
-                              className="form-checkbox"
-                            />
-                            <span className="ml-2">{subSubcat}</span>
-                          </label>
-                        </div>
-                      ))}
-                    </div>
-                  ))
-                : filter.subcategories.map((subcategory) => (
-                    <div key={subcategory} className="ml-4 mt-2">
-                      <label className="inline-flex items-center justify-start">
-                        <input
-                          type="checkbox"
-                          value={subcategory}
-                          checked={selectedFilters.includes(subcategory)}
-                          onChange={(e) => onFilterChange(e.target.value)}
-                          className="form-checkbox"
-                        />
-                        <span className="ml-2">{subcategory}</span>
-                      </label>
-                    </div>
-                  ))}
             </div>
-          )}
-        </div>
-      ))}
+            {openCategories[filter.category] && (
+              <div className="ml-4 mt-2">
+                {filter.subcategories.map((subcategory) => (
+                  <div key={subcategory} className="ml-4 mt-2">
+                    <label className="inline-flex items-start">
+                      <input
+                        type="checkbox"
+                        value={subcategory}
+                        checked={selectedFilters.includes(subcategory)}
+                        onChange={(e) => onFilterChange(e.target.value)}
+                        className="form-checkbox"
+                      />
+                      <span className="ml-2">{subcategory}</span>
+                    </label>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
