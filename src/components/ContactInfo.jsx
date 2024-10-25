b038f594521f64232dd31e0c079f78c1a58afcf0
jo33200 <jo33200@gmail.com>
2024-10-25 10:22:42 +0200

    Refactor Content-Security-Policy meta tag to allow additional image and map sources

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import ContactMap from "./ContactMap";
 
 const ContactInfo = ({ isFooter = false }) => (
   <div className={isFooter ? "" : "mx-auto flex max-w-lg flex-col gap-10 p-8"}>
@@ -21,14 +22,7 @@ const ContactInfo = ({ isFooter = false }) => (
     </nav>
     {/* Intégration de la carte Google Maps via iframe */}
     <div className={`${isFooter ? "" : "flex justify-center"}`}>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d176.73239901182293!2d-0.6080236082763149!3d44.868027578053166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd54d71598cbcc33%3A0xffdb52962ffa1cb8!2sINDENUM%20-%20ind%C3%A9num%20service%20de%20r%C3%A9paration%20de%20t%C3%A9l%C3%A9phones%20portables%2C%20smartphones%20et%20consoles%20de%20jeux%20vid%C3%A9o!5e0!3m2!1sfr!2sfr!4v1726826599355!5m2!1sfr!2sfr"
-        className={`border-0 ${isFooter ? "h-auto w-[250px] pt-3" : "h-200 w-300 left-0 top-0 lg:h-80 lg:w-full"}`}
-        allowFullScreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-        title="Google Maps"
-      ></iframe>
+      <ContactMap />
     </div>
   </div>
 );
