32cde62a521ce835018c7c818db0de66d94a23b5
jo33200 <jo33200@gmail.com>
2024-10-30 11:16:13 +0100

    Refactor ButtonValid component to accept onClick prop and change button type to button

--- src/components/ButtonValid.jsx
+++ src/components/ButtonValid.jsx
@@ -1,10 +1,12 @@
 import { FaPaperPlane } from "react-icons/fa";
+import PropTypes from 'prop-types';
 
-const Button =() => {
+const ButtonValid =({onClick}) => {
     return (
       <div className="flex justify-center">
           <button
-            type="submit"
+            type="button"
+            onClick={onClick}
             className="flex items-center justify-center rounded-md bg-name-orange px-6 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
             <FaPaperPlane className="mr-2" />
@@ -12,6 +14,10 @@ const Button =() => {
           </button>
         </div>
     );
-}
+};
 
-export default Button;
+ButtonValid.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default ButtonValid;
