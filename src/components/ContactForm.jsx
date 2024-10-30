ed7efd9c8dfc35a6a54b66533ce9d96a7dbc97f4
jo33200 <jo33200@gmail.com>
2024-10-30 10:35:52 +0100

    Refactor ContactForm component to use ButtonValid component for submit button

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaPaperPlane } from "react-icons/fa";
+import ButtonValid from "./ButtonValid";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -94,16 +94,7 @@ const ContactForm = () => {
           />
         </div>
 
-        {/* Bouton Envoyer */}
-        <div className="flex justify-center">
-          <button
-            type="submit"
-            className="flex items-center justify-center rounded-md bg-name-orange px-6 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            <FaPaperPlane className="mr-2" />
-            Envoyer
-          </button>
-        </div>
+        <ButtonValid />
       </form>
     </div>
   );
