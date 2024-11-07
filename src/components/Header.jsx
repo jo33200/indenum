a1b75ba88c39989d3a718b1e75131b22615aa1a5
jo33200 <jo33200@gmail.com>
2024-11-07 12:34:04 +0100

    Add CleTournevis SVG asset and update Header component layout; enhance styles with header border gradient

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../assets/img/Indenum.png";
-import CleTournevis from "../assets/img/cléTournevis.png";
 
 const Header = () => {
   const location = useLocation();
@@ -10,30 +9,24 @@ const Header = () => {
   const getLinkClass = (path) => {
     return `${
       location.pathname === path ? "scale-125" : ""
-    } transition-transform duration-200 hover:scale-110 font-semibold text-white`;
+    } transition-transform duration-200 hover:scale-110 font-semibold text-black`;
   };
 
   return (
-    <header className="h-auto w-full">
-      <div className="flex h-auto w-full flex-row justify-between bg-gradient-to-r from-[#7A6F6F] to-[#583C3C] p-5">
+    <header className="h-auto w-full header-border-gradient">
+      <div className="flex h-auto w-full flex-row justify-between p-2">
         <section className="flex h-full">
-          <div className="hidden lg:block lg:h-full lg:w-20">
-            <img src={CleTournevis} alt="Cle et Tournevis" className="" />
-          </div>
-          <div className="flex flex-col items-center justify-center">
+          <div className="flex items-center justify-center gap-2">
             <img
               src={Logo}
               alt="Indenum"
-              className="h-auto w-48 md:w-60 lg:w-72"
+              className="h-auto w-52"
             />
-            <p className="container w-full text-xs font-semibold text-name-orange sm:text-sm md:text-base">
-              Service de Réparation pour vos objets électroniques
-            </p>
           </div>
         </section>
         <section>
           <nav className="mt-2 w-full">
-            <ul className="flex w-full items-center justify-between px-2 py-2 text-sm sm:text-base md:text-xl lg:w-auto lg:justify-center lg:gap-14 xl:py-3 xl:text-2xl">
+            <ul className="flex w-full items-center justify-between px-2 py-2 text-sm sm:text-base lg:w-auto lg:justify-center lg:gap-10">
               <li className={getLinkClass("/")}>
                 <Link to="/">Accueil</Link>
               </li>
@@ -49,25 +42,19 @@ const Header = () => {
               <li className={getLinkClass("/contact")}>
                 <Link to="/contact">Contact</Link>
               </li>
-            </ul>
-          </nav>
-        </section>
-        <section className="hidden h-full flex-col justify-around gap-4 lg:flex">
-          <ul className="flex h-full w-auto flex-col items-start justify-around text-base font-semibold text-white">
-            <li>
+              <li>
               <a
                 href="mailto:indenum@outlook.com"
                 className="hover:cursor-pointer"
               >
-                <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
-                indenum@outlook.com
+                <FontAwesomeIcon icon={faEnvelope} className="" />
               </a>
             </li>
             <li>
-              <FontAwesomeIcon icon={faPhone} className="mr-2" />
-              07 66 44 13 37
+              <FontAwesomeIcon icon={faPhone} className="" />
             </li>
-          </ul>
+            </ul>
+          </nav>
         </section>
       </div>
     </header>
