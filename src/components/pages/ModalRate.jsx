2107f0fd374cb6ccf2a9c788bef1b4d8fbd8eb56
jo33200 <jo33200@gmail.com>
2024-11-15 15:31:09 +0100

    Add close button to ModalRate component for improved user experience

--- src/components/pages/ModalRate.jsx
+++ src/components/pages/ModalRate.jsx
@@ -1,33 +1,43 @@
 import PropTypes from "prop-types";
+import { FaTimes } from "react-icons/fa"; // Importation de l'icône croix
 import images from "../../assets/images";
-
 
 const ModalRate = ({ rate, image, title, onClose }) => {
   if (!rate) return null;
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
       onClick={onClose} // Ferme la modal lorsque l'utilisateur clique à l'extérieur
     >
       <div
-        className="bg-white p-6 rounded-xl w-11/12 max-w-4xl flex flex-col sm:flex-row"
+        className="relative flex w-11/12 max-w-4xl flex-col rounded-xl bg-white p-6 sm:flex-row"
         onClick={(e) => e.stopPropagation()} // Empêche la fermeture lorsque l'on clique à l'intérieur de la modal
       >
+        <div className="absolute right-4 top-4 z-50">
+          <button
+            onClick={onClose} // Ferme la modal lorsqu'on clique sur la croix
+            className="text-gray-500 hover:text-gray-700"
+            aria-label="Fermer"
+          >
+            <FaTimes size={24} />
+          </button>
+        </div>
         <div className="flex-1">
           <img
             src={images[image]}
             alt={title}
-            className="w-full h-auto object-cover rounded"
+            className="h-auto w-full rounded object-cover"
           />
         </div>
-        <div className="flex-1 flex flex-col justify-between px-6 py-4">
+        <div className="flex flex-1 flex-col justify-between px-6 py-4">
           <h3 className="text-2xl font-bold">{rate.title}</h3>
           <p className="text-gray-700">{rate.description}</p>
           <p className="text-sm text-gray-500">{rate.category}</p>
           <p className="text-lg font-semibold">{rate.price}€</p>
           <p className="mt-4 text-sm text-gray-600">
-            Garantie : Nous offrons une garantie de 6 mois sur tous nos produits.
+            Garantie : Nous offrons une garantie de 6 mois sur tous nos
+            produits.
           </p>
         </div>
       </div>
