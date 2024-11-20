9446a3b6154deac40bc7865286118a86503c983f
jo33200 <jo33200@gmail.com>
2024-11-20 15:45:12 +0100

    Update ModalRate component to enhance layout and improve responsiveness

--- src/components/pages/ModalRate.jsx
+++ src/components/pages/ModalRate.jsx
@@ -17,10 +17,10 @@ const ModalRate = ({ rate, image, title, onClose }) => {
 
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+      className="fixed inset-0 mt-16 flex justify-center md:items-center md:bg-black md:bg-opacity-50"
       onClick={onClose} // Ferme la modal lorsque l'utilisateur clique à l'extérieur
     >
-      <div className="mt-32 max-h-screen w-screen sm:h-full sm:mx-6 max-w-4xl sm:relative sm:mt-0 sm:flex sm:max-h-[700px] sm:justify-center sm:p-7">
+      <div className="max-h-screen w-screen max-w-4xl overflow-hidden md:relative md:mx-6 md:mt-0 md:flex md:h-full md:max-h-[700px] md:justify-center md:p-7">
         <div className="absolute right-4 top-4 z-50 hidden rounded-full border-4 border-white p-1 text-center hover:bg-red-600 hover:text-black md:right-0 md:top-0 md:flex">
           <button
             onClick={onClose} // Ferme la modal lorsqu'on clique sur la croix
@@ -31,37 +31,39 @@ const ModalRate = ({ rate, image, title, onClose }) => {
           </button>
         </div>
         <div
-          className="relative flex md:hidden h-full w-full flex-col items-center bg-white p-4 sm:p-6 sm:flex-row md:gap-10 md:rounded-md md:p-10"
+          className="relative flex h-full w-full flex-col items-center justify-between overflow-auto bg-white p-4 sm:p-6 md:flex-row md:gap-5 md:rounded-md md:p-4"
           onClick={(e) => e.stopPropagation()} // Empêche la fermeture lorsque l'on clique à l'intérieur de la modal
         >
-          <div className="w-full">
-            <button className="flex justify-center items-center gap-1 pb-4">
-            <HiChevronDoubleLeft
-              size={20}
-              onClick={onClose} // Ferme la modal lorsqu'on clique sur la croix
-              className="block sm:hidden"
-            />
-            <p onClick={onClose} >Revenir en arrière</p>
-          </button>
+          <div className="w-full sm:w-3/4 md:hidden">
+            <button className="justify-beetwen flex items-center gap-1 pb-4">
+              <HiChevronDoubleLeft
+                size={20}
+                onClick={onClose} // Ferme la modal lorsqu'on clique sur la croix
+                className="block text-gray-500"
+              />
+              <p onClick={onClose}>Revenir en arrière</p>
+            </button>
           </div>
-          
+
           <section className="flex justify-center md:flex-1">
             <img
               src={images[image]}
               alt={title}
-              className="h-auto w-3/4 md:w-full border object-cover px-3 py-6 md:px-10 md:py-20"
+              className="h-auto w-3/4 border object-cover px-3 py-6 md:w-full md:px-6 md:py-20"
             />
           </section>
-          <section className="flex flex-1 flex-col items-start justify-between gap-6 px-6 py-4">
-            <h3 className="w-full text-2xl text-center md:text-left font-bold">{rate.title}</h3>
+          <section className="flex flex-col items-start justify-between gap-6 px-6 py-1 sm:w-3/4 md:w-1/2 md:px-2 md:gap-auto md:h-full">
+            <h3 className="w-full text-center text-2xl font-bold md:text-left lg:text-center">
+              {rate.title}
+            </h3>
             <p className="text-base text-gray-700">{rate.description}</p>
-            <p className="text-base text-gray-500">
+            <p className="text-sm text-gray-500 md:text-base">
               catégorie: {rate.category}
             </p>
-            <p className="w-full rounded-3xl border-2 py-2 text-2xl font-bold">
+            <p className="w-full rounded-3xl border-2 text-2xl font-bold md:py-2">
               {rate.price}€
             </p>
-            <p className="mt-4 text-lg text-gray-600">
+            <p className="text-gray-600 md:mt-4 md:text-lg">
               <strong>
                 Garantie : Nous offrons une garantie de 6 mois sur tous nos
                 produits.
@@ -71,7 +73,9 @@ const ModalRate = ({ rate, image, title, onClose }) => {
               Tous nos prix comprennent la pièce et la main d’oeuvre
             </p>
             <div className="flex w-full flex-col gap-4">
-              <h3>Moyens de paiement sur place :</h3>
+              <h4 className="text-sm md:text-base">
+                Moyens de paiement sur place :
+              </h4>
               <div className="flex w-full items-center justify-between">
                 <img src={Visa} alt="Visa" className="w-12" />
                 <img src={Mastercard} alt="Mastercard" className="w-12" />
@@ -80,7 +84,9 @@ const ModalRate = ({ rate, image, title, onClose }) => {
                 <img src={Paypal} alt="Paypal" className="w-12" />
                 <img src={GooglePay} alt="GooglePay" className="w-12" />
               </div>
-              <p className="text-sm font-semibold">Espèces acceptées</p>
+              <p className="text-sm font-semibold lg:text-base">
+                Espèces acceptées
+              </p>
             </div>
           </section>
         </div>
