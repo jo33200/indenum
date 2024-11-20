885ddb6589ceda65bbe99ae2dd8b7b912944a0bd
jo33200 <jo33200@gmail.com>
2024-11-20 15:57:14 +0100

    Update Contact component to enhance layout by adjusting item alignment

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -3,7 +3,7 @@ import ContactInfo from "../../components/common/ContactInfo";
 
 const Contact = () => {
   return (
-    <div className="my-16 flex h-auto w-full flex-col items-center justify-center gap-10 md:my-0 lg:min-h-screen lg:flex-row lg:justify-around xl:w-2/3 xl:justify-between">
+    <div className="my-16 flex h-auto w-full flex-col items-center justify-center gap-10 md:my-0 lg:min-h-screen lg:items-start lg:flex-row lg:justify-around">
       <section className="mt-5 w-full">
         <ContactInfo />
       </section>
